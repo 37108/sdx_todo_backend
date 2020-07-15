8e9f893b3b6778a1e014fd4365b0d4a30cbf0ae5
37108 <37108mail@gmail.com>
2020-07-15 11:20:31 +0900

    add api

--- lib/backend.ts
+++ lib/backend.ts
@@ -1,21 +1,42 @@
-import * as ddb from '@aws-cdk/aws-dynamodb';
+// CfnAuthorizer
+import { HttpApi, HttpMethod, LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2';
+import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const tasksTable = new ddb.Table(this, 'tasks', {
+    const tasksTable = new Table(this, 'tasks', {
       tableName: 'tasks',
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       partitionKey: {
         name: 'user',
-        type: ddb.AttributeType.STRING,
+        type: AttributeType.STRING,
       },
       sortKey: {
         name: 'id',
-        type: ddb.AttributeType.STRING,
+        type: AttributeType.STRING,
       },
+    })
+
+
+    const getTasks = new Function(this, 'getTasksFunction', {
+      runtime: Runtime.NODEJS_12_X,
+      handler: 'index.handler',
+      code: Code.fromAsset('./'),
+    })
+
+    const getTasksInteg = new LambdaProxyIntegration({
+      handler: '',
+    })
+
+    const api = new HttpApi(this, 'api', {})
+    api.addRoutes({
+      path: '/tasks',
+      methods: [ HttpMethod.GET],
+      integration: '',
     })
   }
 }
