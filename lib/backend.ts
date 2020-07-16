8f6d4140d429f509f97b6b3d99e87819a997eafa
37108 <37108mail@gmail.com>
2020-07-16 11:48:52 +0900

    add api gateway

--- lib/backend.ts
+++ lib/backend.ts
@@ -1,8 +1,8 @@
-// CfnAuthorizer
-import { HttpApi, HttpMethod, LambdaProxyIntegration } from '@aws-cdk/aws-apigatewayv2';
+import { HttpApi, HttpMethod, LambdaProxyIntegration /**CfnAuthorizer**/ } from '@aws-cdk/aws-apigatewayv2';
 import { AttributeType, Table } from '@aws-cdk/aws-dynamodb';
 import { Code, Function, Runtime } from '@aws-cdk/aws-lambda';
 import * as cdk from '@aws-cdk/core';
+import { resolve } from 'path';
 
 export class BackendStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -25,18 +25,18 @@ export class BackendStack extends cdk.Stack {
     const getTasks = new Function(this, 'getTasksFunction', {
       runtime: Runtime.NODEJS_12_X,
       handler: 'index.handler',
-      code: Code.fromAsset('./'),
+      code: Code.fromAsset(resolve(__dirname, '../lambda/tasks_by_user/'),),
     })
 
-    const getTasksInteg = new LambdaProxyIntegration({
-      handler: '',
+    const getTasksIntegration = new LambdaProxyIntegration({
+      handler: getTasks,
     })
 
     const api = new HttpApi(this, 'api', {})
     api.addRoutes({
-      path: '/tasks',
+      path: '/task',
       methods: [ HttpMethod.GET],
-      integration: '',
+      integration: getTasksIntegration,
     })
   }
 }
